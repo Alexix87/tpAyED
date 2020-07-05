56305383525876a3961ae2733130dd298f5be8ba
Alexix87 <alexis.cabo.87@gmail.com>
2020-07-05 19:10:02 -0300

    Abre y cierra los archivos de texto de entrada

--- main.cpp
+++ main.cpp
@@ -1,62 +1,32 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdlib>
 
 using namespace std;
-const int MAX_COUNTRY = 20;
+const int MAX_PAIS = 200;
 
+void Abrir (FILE* &Paises, FILE* &ParteDiario){
+    Paises = fopen("Paises.txt","r");
+    ParteDiario = fopen("ParteDiario.txt","r");
+}
 
-struct country {
-    string countryName;
-    string continent;
-    int people;
-    void print() const {
-        cout << " Pais: " << countryName << " - ";
-        cout << " Continente: "<< continent << " - ";
-        cout << " Personas: "<< people << endl;
-    }
-};
-
-
-void splitLineOfCountry(string const line, string countryArray []){
-    string newLine;
-    int len, space;
-    space  = line.find(" ");
-    len  = line.length();
-    countryArray[0] = line.substr(0, space);
-    space = line.find(" ");
-    newLine = line.substr(space + 1, len+1);
-    countryArray[1] = newLine.substr(0 ,newLine.find(" "));
-    space = newLine.find(" ");
-    len = newLine.length();
-    countryArray[2] = newLine.substr(space,len);
-};
-
-void readCountryFile(ifstream &file, struct country countries[]){
-    string countryArray[3];
-    string line;
-    int i;
-    for (i = 0; i < MAX_COUNTRY; i++) {
-        getline(file, line);
-        splitLineOfCountry(line, countryArray);
-        countries[i].countryName = countryArray[0];
-        countries[i].continent = countryArray[1];
-        countries[i].people = atoi(countryArray[2].c_str());
-        countries[i].print();
-    };
+void Cerrar(FILE* Paises, FILE* ParteDiario){
+    fclose(Paises);
+    fclose(ParteDiario);
 }
 
 
 int main() {
-    ifstream MyReadFile("/home/manu/UTN/TP1New/Paises.txt");
-    country countries[MAX_COUNTRY];
-    cout << "Generando Archivos..." << endl;
-    if (MyReadFile.is_open()) {
-        readCountryFile(MyReadFile, countries);
-    } else {
-        cout << "No se pudo abrir el archivo";
-    }
-    MyReadFile.close();
+    FILE *Paises;
+    FILE *ParteDiario;
+
+    Abrir (Paises, ParteDiario);
+  
+    //freopen(nomFisArchivo, modo, stdin);
+    //ProcPaises(argumentos...);
+    //ProcParteDiario(Argumentos...);
+    //Listado(argumentos...); 
+    // Desntro del modulo listado debe exstir un freopen(nomFisArchivo,modo,stdout)
+    //freopen("CON", modo, stdin);
+    
+    Cerrar(Paises,ParteDiario);
     return 0;
 }
